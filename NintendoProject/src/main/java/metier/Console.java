e449922c814a62db1619753ea185eeb19c635947
Gianni <galatigianni@gmail.com>
2021-05-10 17:43:05 +0200

    ajout prix et date de sortie console

--- NintendoProject/src/main/java/metier/Console.java
+++ NintendoProject/src/main/java/metier/Console.java
@@ -1,25 +1,41 @@
 package metier;
 
+import java.time.LocalDate;
+
 public class Console {
 
 	private String nom;
-
-	public Console(String nom) {
+	private int Prix;
+	private LocalDate dateDeSortie;
+	public Console(String nom, int prix, LocalDate dateDeSortie) {
+		super();
 		this.nom = nom;
+		Prix = prix;
+		this.dateDeSortie = dateDeSortie;
 	}
-
 	public String getNom() {
 		return nom;
 	}
-
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
-
+	public int getPrix() {
+		return Prix;
+	}
+	public void setPrix(int prix) {
+		Prix = prix;
+	}
+	public LocalDate getDateDeSortie() {
+		return dateDeSortie;
+	}
+	public void setDateDeSortie(LocalDate dateDeSortie) {
+		this.dateDeSortie = dateDeSortie;
+	}
 	@Override
 	public String toString() {
-		return "Console [nom=" + nom + "]";
+		return "Console [nom=" + nom + ", Prix=" + Prix + ", dateDeSortie=" + dateDeSortie + "]";
 	}
+
 	
 	
 }
