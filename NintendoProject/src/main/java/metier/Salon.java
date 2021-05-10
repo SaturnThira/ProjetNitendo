1818d16cbaa6503beb188c3bd558e5a58e6a5aa0
Bcoding900 <mail>
2021-05-10 18:00:07 +0200

    ajout des filles de la classe console

---
+++ NintendoProject/src/main/java/metier/Salon.java
@@ -0,0 +1,13 @@
+package metier;
+
+public class Salon extends Console{
+
+	public Salon(String nom, int prix, String dateDeSortie) {
+		super(nom, prix, dateDeSortie);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+
+}
