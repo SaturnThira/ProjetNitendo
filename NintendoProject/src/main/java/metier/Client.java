d7bb7906cfa618951dff45d433c4ad20f715d2e9
Gianni <galatigianni@gmail.com>
2021-05-10 17:03:17 +0200

    update class client

--- NintendoProject/src/main/java/metier/Client.java
+++ NintendoProject/src/main/java/metier/Client.java
@@ -6,5 +6,29 @@ public class Client {
       private String nom;
       private String prenom;
         List<String> ListedeJeux ;
+		public Client(String nom, String prenom, List<String> listedeJeux) {
+			super();
+			this.nom = nom;
+			this.prenom = prenom;
+			ListedeJeux = listedeJeux;
+		}
+		public String getNom() {
+			return nom;
+		}
+		public void setNom(String nom) {
+			this.nom = nom;
+		}
+		public String getPrenom() {
+			return prenom;
+		}
+		public void setPrenom(String prenom) {
+			this.prenom = prenom;
+		}
+		public List<String> getListedeJeux() {
+			return ListedeJeux;
+		}
+		public void setListedeJeux(List<String> listedeJeux) {
+			ListedeJeux = listedeJeux;
+		}
       
 }
